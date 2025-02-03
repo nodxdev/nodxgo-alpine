131041ca4875c7f35e50cc0e7a2a288093daec36
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-03 20:52:36 +0000

    Add Alpine.js directive attributes and examples to alpine.go and alpine_test.go

--- alpine.go
+++ alpine.go
@@ -8,27 +8,138 @@ import nodx "github.com/nodxdev/nodxgo"
 
 // Template is a generic function for rendering a <template> element.
 //
-//	func ExampleTemplate() {
-//		node := alpine.Template(
-//			nodx.Div(nodx.Text("Div")),
-//			nodx.SpanEl(nodx.Text("Span")),
-//		)
-//		fmt.Println(node)
-//		// Output: <template><div>Div</div><span>Span</span></template>
-//	}
+// Often used with x-for and x-if directives.
 func Template(children ...nodx.Node) nodx.Node {
 	return nodx.El("template", children...)
 }
 
 // X is an attribute that renders a x-[key]="[value]" attribute.
-//
-//	func ExampleX() {
-//		node := nodx.Div(
-//			alpine.X("if", "true"),
-//		)
-//		fmt.Println(node)
-//		// Output: <div x-if="true"></div>
-//	}
 func X(key string, value string) nodx.Node {
 	return nodx.Attr("x-"+key, value)
+}
+
+// XData is an attribute that renders a x-data="[value]" attribute.
+//
+// https://alpinejs.dev/directives/data
+func XData(value string) nodx.Node {
+	return X("data", value)
+}
+
+// XInit is an attribute that renders a x-init="[value]" attribute.
+//
+// https://alpinejs.dev/directives/init
+func XInit(value string) nodx.Node {
+	return X("init", value)
+}
+
+// XShow is an attribute that renders a x-show="[vlue]" attribute.
+//
+// https://alpinejs.dev/directives/show
+func XShow(value string) nodx.Node {
+	return X("show", value)
+}
+
+// XBind is an attribute that renders a x-bind:[targetAttr]="[value]" attribute.
+//
+// https://alpinejs.dev/directives/bind
+func XBind(targetAttr string, value string) nodx.Node {
+	return X("bind:"+targetAttr, value)
+}
+
+// XOn is an attribute that renders a x-on:[targetEvent]="[value]" attribute.
+//
+// https://alpinejs.dev/directives/on
+func XOn(targetEvent string, value string) nodx.Node {
+	return X("on:"+targetEvent, value)
+}
+
+// XText is an attribute that renders a x-text="[value]" attribute.
+//
+// https://alpinejs.dev/directives/text
+func XText(value string) nodx.Node {
+	return X("text", value)
+}
+
+// XHTML is an attribute that renders a x-html="[value]" attribute.
+//
+// https://alpinejs.dev/directives/html
+func XHTML(value string) nodx.Node {
+	return X("html", value)
+}
+
+// XModel is an attribute that renders a x-model="[value]" attribute.
+//
+// https://alpinejs.dev/directives/model
+func XModel(value string) nodx.Node {
+	return X("model", value)
+}
+
+// XModelable is an attribute that renders a x-modelable="[value]" attribute.
+//
+// https://alpinejs.dev/directives/modelable
+func XModelable(value string) nodx.Node {
+	return X("modelable", value)
+}
+
+// XFor is an attribute that renders a x-for="[value]" attribute.
+//
+// https://alpinejs.dev/directives/for
+func XFor(value string) nodx.Node {
+	return X("for", value)
+}
+
+// XTransition is an attribute that renders a x-transition attribute.
+//
+// https://alpinejs.dev/directives/transition
+func XTransition() nodx.Node {
+	return X("transition", "")
+}
+
+// XEffect is an attribute that renders a x-effect="[value]" attribute.
+//
+// https://alpinejs.dev/directives/effect
+func XEffect(value string) nodx.Node {
+	return X("effect", value)
+}
+
+// XIgnore is an attribute that renders a x-ignore attribute.
+//
+// https://alpinejs.dev/directives/ignore
+func XIgnore() nodx.Node {
+	return X("ignore", "")
+}
+
+// XRef is an attribute that renders a x-ref="[value]" attribute.
+//
+// https://alpinejs.dev/directives/ref
+func XRef(value string) nodx.Node {
+	return X("ref", value)
+}
+
+// XCloak is an attribute that renders a x-cloak attribute.
+//
+// https://alpinejs.dev/directives/cloak
+func XCloak() nodx.Node {
+	return X("cloak", "")
+}
+
+// XTeleport is an attribute that renders a x-teleport="[value]" attribute.
+//
+// https://alpinejs.dev/directives/teleport
+func XTeleport(value string) nodx.Node {
+	return X("teleport", value)
+}
+
+// XIf is an attribute that renders a x-if="[value]" attribute.
+//
+// https://alpinejs.dev/directives/if
+func XIf(value string) nodx.Node {
+	return X("if", value)
+}
+
+// XId is an attribute that renders a x-id="[value]" attribute.
+//
+// https://alpinejs.dev/directives/id
+func XId(value string) nodx.Node {
+	return X("id", value)
 }
--- alpine_test.go
+++ alpine_test.go
@@ -23,3 +23,147 @@ func ExampleX() {
 	fmt.Println(node)
 	// Output: <div x-if="true"></div>
 }
+
+func ExampleXData() {
+	node := nodx.Div(
+		alpine.XData(`{}`),
+	)
+	fmt.Println(node)
+	// Output: <div x-data="{}"></div>
+}
+
+func ExampleXInit() {
+	node := nodx.Div(
+		alpine.XInit("console.log(true)"),
+	)
+	fmt.Println(node)
+	// Output: <div x-init="console.log(true)"></div>
+}
+
+func ExampleXShow() {
+	node := nodx.Div(
+		alpine.XShow("true"),
+	)
+	fmt.Println(node)
+	// Output: <div x-show="true"></div>
+}
+
+func ExampleXBind() {
+	node := nodx.Div(
+		alpine.XBind("attr", "value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-bind:attr="value"></div>
+}
+
+func ExampleXOn() {
+	node := nodx.Div(
+		alpine.XOn("click", "console.log(true)"),
+	)
+	fmt.Println(node)
+	// Output: <div x-on:click="console.log(true)"></div>
+}
+
+func ExampleXText() {
+	node := nodx.Div(
+		alpine.XText("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-text="value"></div>
+}
+
+func ExampleXHTML() {
+	node := nodx.Div(
+		alpine.XHTML("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-html="value"></div>
+}
+
+func ExampleXModel() {
+	node := nodx.Div(
+		alpine.XModel("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-model="value"></div>
+}
+
+func ExampleXModelable() {
+	node := nodx.Div(
+		alpine.XModelable("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-modelable="value"></div>
+}
+
+func ExampleXFor() {
+	node := nodx.Div(
+		alpine.XFor("color in colors"),
+	)
+	fmt.Println(node)
+	// Output: <div x-for="color in colors"></div>
+}
+
+func ExampleXTransition() {
+	node := nodx.Div(
+		alpine.XTransition(),
+	)
+	fmt.Println(node)
+	// Output: <div x-transition=""></div>
+}
+
+func ExampleXEffect() {
+	node := nodx.Div(
+		alpine.XEffect("console.log(label)"),
+	)
+	fmt.Println(node)
+	// Output: <div x-effect="console.log(label)"></div>
+}
+
+func ExampleXIgnore() {
+	node := nodx.Div(
+		alpine.XIgnore(),
+	)
+	fmt.Println(node)
+	// Output: <div x-ignore=""></div>
+}
+
+func ExampleXRef() {
+	node := nodx.Div(
+		alpine.XRef("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-ref="value"></div>
+}
+
+func ExampleXCloak() {
+	node := nodx.Div(
+		alpine.XCloak(),
+	)
+	fmt.Println(node)
+	// Output: <div x-cloak=""></div>
+}
+
+func ExampleXTeleport() {
+	node := nodx.Div(
+		alpine.XTeleport("body"),
+	)
+	fmt.Println(node)
+	// Output: <div x-teleport="body"></div>
+}
+
+func ExampleXIf() {
+	node := nodx.Div(
+		alpine.XIf("true"),
+	)
+	fmt.Println(node)
+	// Output: <div x-if="true"></div>
+}
+
+func ExampleXId() {
+	node := nodx.Div(
+		alpine.XId("value"),
+	)
+	fmt.Println(node)
+	// Output: <div x-id="value"></div>
+}
